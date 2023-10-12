bc468cd22a434fd1a8836a09db3bcf79a47386bf
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-12 18:02:03 -0500

    Update Project

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Main",
+    page_icon="👋",
+)
+
+ßdawda
+
+st.write("# ¡Bienvenido a Machine Learning Projects! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+¡Hola a todos! Soy Mateo, un entusiasta apasionado por el mundo de la inteligencia artificial y el aprendizaje automático. Este es mi espacio personal donde compartiré mi viaje de aprendizaje en el emocionante campo de Machine Learning.
+
+Es importante destacar que aún estoy en las primeras etapas de mi aventura en el mundo del machine learning, y esta página es un registro de mis descubrimientos, éxitos y desafíos a medida que avanzo en este emocionante camino. ¿Por qué comparto esto? Porque creo que la mejor manera de aprender es haciéndolo juntos, aprendiendo de nuestros errores y celebrando nuestros logros.
+
+En este espacio, exploraremos juntos conceptos clave, proyectos prácticos, y exploraremos los diversos modelos de machine learning que estoy descubriendo. Mi objetivo es no solo mejorar mis habilidades, sino también ayudar a otros a entender mejor este fascinante campo.
+
+Así que, bienvenidos a este viaje. ¡Prepárense para sumergirse en el mundo del machine learning conmigo y explorar juntos las maravillas que este campo tiene para ofrecer!
+
+¡Sigamos aprendiendo, experimentando y construyendo juntos!
+
+"""
+)
