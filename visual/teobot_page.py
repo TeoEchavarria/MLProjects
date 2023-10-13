b85db382d95311141fd69e387dc7efb18a2ad8c6
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-13 16:34:10 -0500

    Requirements and init Teo Bot

---
+++ visual/teobot_page.py
@@ -0,0 +1,19 @@
+from langchain.llms import CTransformers
+from langchain.chains import QAGenerationChain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.docstore.document import Document
+from langchain.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.summarize import load_summarize_chain
+from langchain.chains import RetrievalQA
+
+import os 
+import json
+import time
+
+
+class TeoBotPage:
+    def __init__(self) -> None:
+        pass
