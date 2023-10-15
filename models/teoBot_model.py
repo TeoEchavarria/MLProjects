887af5c6e93978a1d1d6938d2ece43526ee4c36d
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-15 18:57:00 -0500

    Update TeoBot

---
+++ models/teoBot_model.py
@@ -0,0 +1,19 @@
+from ctransformers import AutoModelForCausalLM, AutoTokenizer
+
+class TeoBotModel:
+    def __init__(self):
+        #self.llm = ctransformers(
+        #model = "thebloke/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
+        #model_type="mistral",
+        #max_new_tokens = 1048,
+        #temperature = 0.3
+        self.llm = AutoModelForCausalLM.from_pretrained(
+            "TheBloke/Mistral-7B-Instruct-v0.1-GGUF", 
+            model_file="mistral-7b-instruct-v0.1.Q4_K_M.gguf", 
+            model_type="mistral", 
+            gpu_layers=50
+            )
+     
+    def generate_response(self, question):
+        return self.llm(question)
+
--- visual/teobot_page.py
+++ visual/teobot_page.py
@@ -1,19 +1,46 @@
-from langchain.llms import CTransformers
-from langchain.chains import QAGenerationChain
-from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.docstore.document import Document
-from langchain.document_loaders import PyPDFLoader
-from langchain.prompts import PromptTemplate
-from langchain.embeddings import HuggingFaceBgeEmbeddings
-from langchain.vectorstores import FAISS
-from langchain.chains.summarize import load_summarize_chain
-from langchain.chains import RetrievalQA
+import streamlit as st
 
-import os 
-import json
-import time
+from models.teoBot_model import TeoBotModel
 
+model = TeoBotModel()
 
 class TeoBotPage:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        
+        # Escritura de primera interaccion con el chat
+        if "messages" not in st.session_state.keys():
+            st.session_state.messages = [{"role": "assistant", "content": "Como puedo ayudarte hoy?"}]
+        
+        # Escritura de todas las preguntas
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.write(message["content"])
+        
+        # Boton para limpiar el chat
+        st.sidebar.button('Clear Chat History', on_click=TeoBotPage.clear_chat_history)
+
+        # Creacion de la barra de preguntas y guardarlas
+        if prompt := st.chat_input():
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.write(prompt)
+                
+        # Generacion de respuesta tanto del chat como de la categoria
+        if st.session_state.messages[-1]["role"] != "assistant":
+            with st.chat_message("assistant"):
+                with st.spinner("Thinking..."):
+                    
+                    response = model.generate_response(prompt)
+                    placeholder = st.empty()
+
+                    placeholder.markdown(response)
+            message = {"role": "assistant", "content": response}
+            st.session_state.messages.append(message)
+            
+    
+    @staticmethod
+    def clear_chat_history():
+        st.session_state.messages = [{"role": "assistant", "content": "Como puedo ayudarte hoy?"}]
+
+
+
