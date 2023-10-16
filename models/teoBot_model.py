f15461682a9a9344da2aff4996d767f0a0fbc7bf
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-15 19:18:31 -0500

    Update teoBot_model.py
    - Model Update

--- models/teoBot_model.py
+++ models/teoBot_model.py
@@ -9,9 +9,9 @@ class TeoBotModel:
         #temperature = 0.3
         self.llm = AutoModelForCausalLM.from_pretrained(
             "TheBloke/Mistral-7B-Instruct-v0.1-GGUF", 
-            model_file="mistral-7b-instruct-v0.1.Q4_K_M.gguf", 
+            model_file="mistral-7b-instruct-v0.1.Q2_K.gguf", 
             model_type="mistral", 
-            gpu_layers=50
+            gpu_layers=40
             )
      
     def generate_response(self, question):
