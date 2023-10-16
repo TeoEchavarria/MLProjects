e4c551f7976c05398cc635a97c52c8f9f6ea6f34
TeoEchavarria <echavarria.0042@gmail.com>
2023-10-16 12:36:10 -0500

    Update teoBot_model.py
    Use st.cache_data

--- models/teoBot_model.py
+++ models/teoBot_model.py
@@ -2,20 +2,20 @@ from ctransformers import AutoModelForCausalLM, AutoTokenizer
 import streamlit as st
 
 class TeoBotModel:
-    @st.cache
-    def __init__(self):
+    @st.cache_data
+    def __init__(_self):
         #self.llm = ctransformers(
         #model = "thebloke/mistral-7b-instruct-v0.1.Q4_K_M.gguf",
         #model_type="mistral",
         #max_new_tokens = 1048,
         #temperature = 0.3
-        self.llm = AutoModelForCausalLM.from_pretrained(
+        _self.llm = AutoModelForCausalLM.from_pretrained(
             "TheBloke/Mistral-7B-Instruct-v0.1-GGUF", 
             model_file="mistral-7b-instruct-v0.1.Q2_K.gguf", 
             model_type="mistral", 
             gpu_layers=40
             )
      
-    def generate_response(self, question):
-        return self.llm(question)
+    def generate_response(_self, question):
+        return _self.llm(question)
 
